a3e1921c16d8751a309e0e772f0ae05fb4b47d94
darriusdupree-wq <darrius.dupree@ascte.org>
2026-09-22 08:27:42 -0500

    Update playlist.py

--- playlist.py
+++ playlist.py
@@ -4,6 +4,7 @@ SONGS = [
     ("Imagine", "John Lennon"),
     ("Blinding Lights", "The Weeknd"),
     ("Viva La Vida", "Coldplay"),
+    ("Last Night", "Morgan Wallen")
 ]
 
 def print_playlist(songs):
